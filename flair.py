b5f28609663ee592f9aa27e0ad5f143ddadad407
Daniel <dfoles@bellsouth.net>
2019-08-22 10:51:05 -0400

    Update flair.py

--- flair.py
+++ flair.py
@@ -1,16 +1,18 @@
+import urllib
+import requests
 import re
 import json
 import praw
 import time
 import datetime
-import os.path
 
 debug = False
 
-# Gets the absolute path to the directory in which the script exists, then adds a trailing /
-fdir = os.path.abspath(os.path.dirname(__file__)) + '/'
-
-f = open(fdir + "config.txt", "r")
+sub_name = "steelbookswap"
+sub_name = "pkmntcgtrades"
+sub_name = "vinylcollectors"
+sub_name = 'mousemarket'
+f = open("config/" + sub_name + "-config.txt", "r")
 info = f.read().splitlines()
 f.close()
 
@@ -19,66 +21,197 @@ client_id = info[1]
 client_secret = info[2]
 bot_username = info[3]
 bot_password = info[4]
+try:
+	swap_word = " " + info[5]
+except:
+	swap_word = " Swaps"
+FNAME_comments = 'database/active_comments-' + subreddit_name + '.txt'
+FNAME_swaps = 'database/swaps-' + subreddit_name + ".json"
+FNAME_archive = 'database/archive-' + subreddit_name + '.txt'
 
-fc = open(fdir + "flairconfig.txt", "r")
-flairtemp = fc.read().splitlines()
-fc.close()
+# IDK, I needed this according to stack overflow.
+def ascii_encode_dict(data):
+        ascii_encode = lambda x: x.encode('ascii') if isinstance(x, unicode) else x
+        return dict(map(ascii_encode, pair) for pair in data.items())
 
-def update_flair(sub):
-    flairs = sub.flair(limit=None)
-    # Loop over each author and change their flair
-    for flair in flairs:
-        css = flair['flair_text']
-        css = css.strip(' Swaps')   #Strips ' Swaps' from the string leaving only a number
-        if 0 <= int(css) < 5:
-            template = flairtemp[0]
-        elif 5 <= int(css) < 10:
-            template = flairtemp[1]
-        elif 10 <= int(css) < 20:
-            template = flairtemp[2]
-        elif 20 <= int(css) < 30:
-            template = flairtemp[3]
-        elif 30 <= int(css) < 40:
-            template = flairtemp[4]
-        elif 40 <= int(css) < 50:
-            template = flairtemp[5]
-        elif 50 <= int(css) < 60:
-            template = flairtemp[6]
-        elif 60 <= int(css) < 70:
-            template = flairtemp[7]
-        elif 70 <= int(css) < 80:
-            template = flairtemp[8]
-        elif 80 <= int(css) < 90:
-            template = flairtemp[9]
-        elif 90 <= int(css) < 100:
-            template = flairtemp[10]
-        elif 100 <= int(css) < 200:
-            template = flairtemp[11]
-        elif 200 <= int(css) < 300:
-            template = flairtemp[12]
-        elif 300 <= int(css) < 400:
-            template = flairtemp[13]
-        elif 400 <= int(css) < 500:
-            template = flairtemp[14]
-        elif 500 <= int(css) < 600:
-            template = flairtemp[15]
-        elif 600 <= int(css) < 700:
-            template = flairtemp[16]
-        elif 700 <= int(css) < 800:
-            template = flairtemp[17]
-        elif 800 <= int(css) < 900:
-            template = flairtemp[18]
-        elif 900 <= int(css):
-            template = flairtemp[19]
-        else:
-            print("Invalid data: " + css)
-            continue
+# Function to load the swap DB into memory
+def get_swap_data():
+        with open(FNAME_swaps) as json_data: # open the funko-shop's data
+                funko_store_data = json.load(json_data, object_hook=ascii_encode_dict)
+        return funko_store_data
 
-        print(str(flair['user']) + " - " + css + " Swaps")
-        sub.flair.set(str(flair['user']).lower(), css+" Swaps", flair_template_id=template)
+def update_database(author1, author2, swap_data, message):
+        author1 = str(author1).lower()  # Create strings of the user names for keys and values
+        author2 = str(author2).lower()
 
-    print("Updating Reddit now...")
+        # Default generic value for swaps
+	message = author2 + " - " + message
+	if author1 not in swap_data:  # If we have not seen this user before in swap, make a new entry for them
+                swap_data[author1] = [message]
+        else:  # If we have seen them before, we want to make sure they didnt already get credit for this swap (same $
+                if message in swap_data[author1]:
+                        return False
+                swap_data[author1].append(message)
+        return True  # If all went well, return true
+
+# Writes the json local file... dont touch this.
+def dump_json(swap_data):
+        with open(FNAME_swaps, 'w') as outfile:  # Write out new data
+                outfile.write(str(json.dumps(swap_data))
+                        .replace("'", '"')
+                        .replace(', u"', ', "')
+                        .replace('[u"', '["')
+                        .replace('{u"', '{"')
+                        .encode('ascii','ignore'))
+
+def reassign_all_flair(sub, data):
+        flairs = sub.flair(limit=None)
+        # Loop over each author and change their flair
+        for flair in flairs:
+		user = str(flair['user']).lower()
+		try:
+			css = str(len(data[user]))
+		except:
+			css = "0"
+		text = flair['flair_text']
+		if text and swap_word in text:
+			continue
+                sub.flair.set(user, css + swap_word, css)
+                print(user + " - " + str(user))
+		return
+
+def update_flair(user, count):
+        sub.flair.set(str(user).lower(), count + swap_word, count)
+        print(user + " - " + count)
+
+def add_all_flair(data, sub):
+	new_data = {}
+	bad_count = 0
+	good_count = 0
+	total_swaps = 0
+	for user in data:
+		count = str(len(data[user]))
+		try:
+			update_flair(user, count)
+			new_data[user] = data[user]
+			good_count += 1
+			total_swaps += int(count)
+		except Exception as e:
+			print(user + " IS NOT A USER")
+			bad_count += 1
+	print("users found: " + str(good_count))
+	print("users removed: " + str(bad_count))
+	print("total swaps: " + str(total_swaps))
+
+def add_feedback_from_posts(reddit, sub, ids):
+	swap_data = get_swap_data()
+	for id in ids:
+		submission = reddit.submission(id=str(id))
+		author1 = str(submission.author)
+		comments = submission.comments
+#		children = []
+#		for comment in comments:
+#			for child in comment.replies:
+#				print(child)
+#				children.append(child)
+		for comment in comments:
+			message = "https://www.reddit.com" + str(urllib.quote(comment.permalink.encode('utf-8'), safe=':/'))
+			author2 = str(comment.author)
+			print(author1 + " -> " + author2)
+			print("    " + message)
+			status = update_database(author1, author2, swap_data, message)
+			if not status:
+				print("Found duplicate post")
+
+#		for comment in children:
+#			print(comment)
+#			message = "https://www.reddit.com" + str(urllib.quote(comment.permalink.encode('utf-8'), safe=':/'))
+#			author2 = str(comment.author)
+#			print(author1 + " -> " + author2)
+#			print("    " + message)
+#			status = update_database(author1, author2, swap_data, message)
+#			if not status:
+#				print("Found duplicate post")
+	        count = str(len(swap_data[author1.lower()]))
+		update_flair(author1.lower(), count)
+	dump_json(swap_data)
+
+
+
+def add_feedback_from_vinylcollectors_posts(reddit, sub):
+	mods = [str(x) for x in sub.moderator()]
+	mods.append('ferricyanide')
+	mods.append('u/ferricyanide')
+	post_ids = ['avq2s4', '9baxki', '7zeg1o', '6vmdr4', '5vzo0d', '53qleg', '4i1q6w']
+#	post_ids = ['6vmdr4', '5vzo0d', '53qleg', '4i1q6w']
+	swap_data = get_swap_data()
+	to_handle_later = []
+	f = open("tmp.txt", 'r')
+	comment_ids = f.read().splitlines()
+	f.close()
+	print("have to look at " + str(len(comment_ids)) + " comments")
+	count = 0
+	for id in comment_ids:
+		count += 1
+		comment = reddit.comment(id)
+		message = "https://www.reddit.com" + str(urllib.quote(comment.permalink.encode('utf-8'), safe=':/'))
+                body = comment.body.lower().replace(">", "").replace("<", "")
+		link_regex = re.compile('(https://www.reddit.com/r/vinylcollectors/comments/.*?/.*?/)')
+		links = link_regex.findall(body)
+		for found in links:
+			body = body.replace(found, "")
+		body = body.replace('"', "").replace("'", "").replace("u/ ", "u/")
+		regex = re.compile('(u\/[A-Za-z0-9_-]+)')
+		matches = regex.findall(body)
+		regex = re.compile('(user\/[A-Za-z0-9_-]+)')
+		matches += [x.replace("user/", "u/") for x in regex.findall(body)]
+		regex = re.compile('user (.+?) ')
+		matches += ["u/"+x for x in regex.findall(body)]
+		author2 = str(comment.author).lower()
+		author1 = "a"
+		print("\n========\n" + body + " -> " + message) #"https://www.reddit.com/r/vinylcollectors.com/comments/" + id + "/-/" + comment.id)
+		if len(body.split(" ")) < 2:
+			continue
+		if len(body) < 22:
+			continue
+		if 'link to thread please?' in body:
+			continue
+		if matches and 'negative' not in body:
+			for author1 in matches:
+				if 'u//u/' in author1:
+					author1 = author1[3:]
+				user = ""
+				if not 'prompt payment' in body and not "am satisfied with" in body and not "positive" in body and not 'postive' in body and not 'received' in body and not 'sold to' in body:
+					user = raw_input(author1 + " >> ")
+				if user:
+					continue
+		                print(author1 + " -> " + str(author2))
+		                print("    " + message)
+                		status = update_database(author1, author2, swap_data, message)
+        			if not status:
+					print("Found duplicate post")
+		else:
+			if str(comment.author) in mods and 'positive' not in body:
+				continue
+			while author1:
+				author1 = raw_input(">> ")
+				if not author1:
+					continue
+				author1 = "u/" + author1.lower()
+		                print(author1 + " -> " + str(author2))
+		                print("    " + message)
+                		status = update_database(author1, author2, swap_data, message)
+        			if not status:
+					print("Found duplicate post")
+		dump_json(swap_data)
+		f = open("tmp.txt", 'w')
+		f.write("\n".join(comment_ids[count:]))
+		f.close()
 
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent='UserAgent', username=bot_username, password=bot_password)
 sub = reddit.subreddit(subreddit_name)
-update_flair(sub)
+
+#add_feedback_from_vinylcollectors_posts(reddit, sub)
+#add_feedback_from_posts(reddit, sub, ['9erx6e', '84hbfq', '5wqjdl', '4yj732'])
+#add_all_flair(get_swap_data(), sub)
+reassign_all_flair(sub, get_swap_data())
